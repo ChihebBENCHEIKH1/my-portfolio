5f37a1956e5683959cd10ee1f918033b6d813be9
chiheb ben cheikh <bencheikh.chiheb@gmail.com>
2026-03-14 19:42:29 +0100

    Updates

--- assets/SceneManager-DzsuVeMj.js
+++ assets/SceneManager-DzsuVeMj.js
@@ -1,4 +1,4 @@
-import{r as He,g as Qy,j as Xt,R as ap,u as $y}from"./index-B1Dcn6mF.js";/**
+import{r as He,g as Qy,j as Xt,R as ap,u as $y}from"./index-eXee15ed.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
