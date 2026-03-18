46da877038c25610084dd55d7639680be6d09cb0
chiheb ben cheikh <bencheikh.chiheb@gmail.com>
2026-03-18 23:48:44 +0100

    Updates

--- assets/SceneManager-KlRYoB5O.js
+++ assets/SceneManager-KlRYoB5O.js
@@ -1,4 +1,4 @@
-import{r as He,g as Qy,j as Xt,R as ap,u as $y}from"./index-J2p8FiWq.js";/**
+import{r as He,g as Qy,j as Xt,R as ap,u as $y}from"./index-BcVIPzEc.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
